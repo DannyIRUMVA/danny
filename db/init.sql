5b91aecd9c4dc247551f905bff947e729eb528fa
DannyIRUMVA <boyg87059@gmail.com>
2026-01-18 09:06:53 +0200

    TM: project setup completion running over docker completely

---
+++ db/init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  description TEXT,
+  assigned_to INTEGER REFERENCES users(id) ON DELETE SET NULL,
+  status TEXT CHECK (status IN ('Pending','In Progress','Completed')) DEFAULT 'Pending',
+  due_date DATE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
